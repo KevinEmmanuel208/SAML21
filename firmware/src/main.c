fbf9c42e5a9fe32ef3c7899ee970efc48ab7db5a
KevinEmmanuel208 <96547499+KevinEmmanuel208@users.noreply.github.com>
2022-01-07 17:45:48 -0600

    version 0.5

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -26,8 +26,7 @@
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include "definitions.h"               // SYS function prototypes
-//#include "tarjet.h"
-//#include "tarjet.h"
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Main Entry Point
@@ -42,16 +41,14 @@ void TIMER_handler(TC_TIMER_STATUS status, uintptr_t context)
 {
     led_status = true;
 }
-/**/// change David      2222222222222222222222222222222
-///*/
+
 int main ( void )
 {
     /* Initialize all modules */
     SYS_Initialize ( NULL );
     
     TC2_TimerStart();
-//LED_turn_off(LED_EQUIPMENT);
-
+    
 TC2_TimerCallbackRegister( TIMER_handler, (uintptr_t)NULL );
 
     while ( true )
